e1dc1e0119b0990a343b187a5d8b36c8c1a008d1
Shivang Kumar <2002shiva.2002@gmail.com>
2025-12-05 11:19:57 +0530

    feat:Created generic exception handler

--- src/main/java/com/common/sdk/services/ResponseHandler.java
+++ src/main/java/com/common/sdk/services/ResponseHandler.java
@@ -1,5 +1,44 @@
 package com.common.sdk.services;
 
+import com.common.sdk.models.APIResponse;
+import com.common.sdk.models.enums.APIResponseCode;
+import com.common.sdk.models.interfaces.GenericApiResponse;
+import com.common.sdk.models.interfaces.ResponseCode;
+import com.common.sdk.utils.CommonUtils;
+
 public class ResponseHandler {
+	
+	public <T> GenericApiResponse<T> ok(T data)
+	{
+		return APIResponse.success(data, APIResponseCode.SUCCESS,CommonUtils.getCurrentRequestId());
+	}
+	
+	public <T> GenericApiResponse<T> ok()
+	{
+		return APIResponse.success(APIResponseCode.SUCCESS,CommonUtils.getCurrentRequestId());
+	}
+	
+	public <T> GenericApiResponse<T> ok(T data,ResponseCode responseCode)
+	{
+		return APIResponse.success(data,responseCode,CommonUtils.getCurrentRequestId());
+	}
+	
+	public GenericApiResponse<Void> ok(ResponseCode responseCode)
+	{
+		return APIResponse.success(responseCode,CommonUtils.getCurrentRequestId());
+	}
+	
+	public GenericApiResponse<Void> failure(ResponseCode responseCode)
+	{
+		return APIResponse.failure(responseCode,CommonUtils.getCurrentRequestId());
+	}
+	public GenericApiResponse<Void> failure(int code,String message)
+	{
+		return APIResponse.failure(code,message,CommonUtils.getCurrentRequestId());
+	}
+	public GenericApiResponse<Void> failure(ResponseCode  responseCode,String message)
+	{
+		return APIResponse.failure(responseCode.getCode(),message,CommonUtils.getCurrentRequestId());
+	}
 
 }
--- src/main/java/com/common/sdk/utils/CommonUtils.java
+++ src/main/java/com/common/sdk/utils/CommonUtils.java
@@ -1,6 +1,15 @@
 package com.common.sdk.utils;
 
+import org.slf4j.MDC;
+
+import com.common.sdk.constants.ApplicationConstants;
+
 public class CommonUtils {
 
 	private CommonUtils() {}
+	
+	public static String getCurrentRequestId()
+	{
+		return MDC.get(ApplicationConstants.CORRELATION_ID);
+	}
 }
